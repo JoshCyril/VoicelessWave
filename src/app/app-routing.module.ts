b845afc335c0441d3c620eaf986c28a52f4c3da5
JPC8 <joshua.cyril88@gmail.com>
2022-09-15 13:50:51 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WrongRouteComponent } from './wrong-route/wrong-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"welcome", component:WelcomeComponent},
+  {path:"chat", component:ChatComponent},
+  {path:"", redirectTo:'welcome', pathMatch: 'full'},
+  {path: '**',pathMatch: 'full',component: WrongRouteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ChatComponent } from './chat/chat.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WrongRouteComponent } from './wrong-route/wrong-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ChatComponent,
+    WrongRouteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
